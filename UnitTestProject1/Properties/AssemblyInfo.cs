63a2f50d1d1ed6577050efea7314ce95c0d5fd05
Jesse Nguyen <c18923@txt.textron.com>
2022-04-22 14:50:02 -0500

    starting unit test

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9b66425-b6d4-4c13-9cb2-5b52da2d2f8f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Agnos;
+using System.Web.Http;
+using System.Collections.Generic;
+using System.Web.Http.Results;
+using System.Net;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new ProductsController();
+            var res = controller.Get();
+        }
+
+        private List<Product> results()
+        {
+            var prods = new List<Product>();
+            prods.Add(new Product { id = "626209e11484eb6966ec76b8", name = "Espresso", price = 3.5 });
+            prods.Add(new Product { id = "62620a241484eb6966ec76b9", name = "cookie", price = 5 });
+            prods.Add(new Product { id = "62620a441484eb6966ec76ba", name = "latte", price = 6.75 });
+            prods.Add(new Product { id = "62622b6fd367d58f8823b3e4", name = "mocha", price = 6.5 });
+            prods.Add(new Product { id = "6262dbaa785621011ccd9b52", name = "blt", price = 11 });
+
+            return prods;
+        }
+    }
+}
